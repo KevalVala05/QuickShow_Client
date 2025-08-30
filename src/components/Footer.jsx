523aa984bcba394e51a5abd573c783ce1453debb
KevalVala05 <keval.vala.05.08@gmail.com>
2025-08-30 12:55:51 +0530

    comman changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="pt-4 text-center text-sm pb-5">
-        Copyright {new Date().getFullYear()} © CodeSpire. All Right Reserved.
+        Copyright {new Date().getFullYear()} © Keval Vala. All Right Reserved.
       </p>
     </footer>
   )
